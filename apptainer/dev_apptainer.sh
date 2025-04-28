ff4ed85293ebba516b3ee2f31642b3a4b56ea62d
Marc Cummings <marccummings@live.dk>
2025-04-28 10:11:19 +0200

    Update dev_apptainer.sh

--- apptainer/dev_apptainer.sh
+++ apptainer/dev_apptainer.sh
@@ -12,12 +12,6 @@ else
     cp example.env apptainer/.env
 fi
 
-# Ensure SERVER_NAME=localhost exists in .env
-if ! grep -q "^SERVER_NAME=" apptainer/.env; then
-     echo "SERVER_NAME=localhost" >> apptainer/.env
-     echo "Added SERVER_NAME=localhost to .env"
-fi
-
 # check if reqired dir exists
 # Define the required structure
 required_dirs=("tmp" "tmp/data" "tmp/ezbids-workdir" "tmp/upload" "tmp/workdir")
@@ -43,12 +37,13 @@ while getopts "d" flag; do
 done
 
 export BRAINLIFE_AUTHENTICATION
+
 # Check Node.js version
 REQUIRED_NODE_VERSION="16"
 CURRENT_NODE_VERSION=$(node -v | cut -d '.' -f 1 | sed 's/v//')
 
 if [ "$CURRENT_NODE_VERSION" -ne "$REQUIRED_NODE_VERSION" ]; then
-    echo "Warning: You are using Node.js version $CURRENT_NODE_VERSION. It is recommended to use version $REQUIRED_NODE."
+    echo "Warning: You are using Node.js version $CURRENT_NODE_VERSION. It is recommended to use version $REQUIRED_NODE_VERSION."
     echo "Please switch to Node.js version $REQUIRED_NODE_VERSION."
 fi
 
@@ -58,10 +53,6 @@ git submodule update --init --recursive
 # Install dependencies for api and ui
 (cd api && npm install)
 (cd ui && npm install)
-
-# Prepare husky (if using it for git hooks)
-npm install husky --save-dev
-npm run prepare-husky
 
 # Generate keys (assuming this is a necessary setup step)
 ./generate_keys.sh
