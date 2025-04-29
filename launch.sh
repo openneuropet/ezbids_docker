a80a4c28538300e8fcdca8120a1d4f7e2b33d269
anthony galassi <28850131+bendhouseart@users.noreply.github.com>
2025-04-29 19:24:54 +0000

    local work, npm is locked down in the containers.

--- launch.sh
+++ launch.sh
@@ -44,20 +44,6 @@ else
   set -e
 fi
 
-# Check Node.js version
-REQUIRED_NODE_VERSION="16"
-CURRENT_NODE_VERSION=$(node -v | cut -d '.' -f 1 | sed 's/v//')
-
-if [ "$CURRENT_NODE_VERSION" -ne "$REQUIRED_NODE_VERSION" ]; then
-    echo "Warning: You are using Node.js version $CURRENT_NODE_VERSION. It is recommended to use version $REQUIRED_NODE_VERSION."
-    echo "Please switch to Node.js version $REQUIRED_NODE_VERSION."
-fi
-
-# build local changes and mount them directly into the containers
-# api/ and ui/ are mounted as volumes  at /app within the docker-compose.yml
-(cd api && npm install) || { echo "npm install failed in api"; exit 1; }
-(cd ui && npm install) || { echo "npm install failed in ui"; exit 1; }
-
 # update the bids submodule
 git submodule update --init --recursive
 
@@ -69,10 +55,17 @@ else
   DOCKER_COMPOSE_FILE=docker-compose.yml
 fi
 
-mkdir -p /tmp/upload
-mkdir -p /tmp/workdir
-
-#npm run prepare-husky
+if [[ ${EZBIDS_TMP_DIR} ]]; then
+  # set working dir to the temp dir specified in above variable
+  EZBIDS_WORKING_DIR=${EZBIDS_TMP_DIR}
+  # check to see if it exists
+else:
+  EZBIDS_WORKING_DIR=/tmp/ezbids-workdir/
+    mkdir ${EZBIDS_TMP_DIR}
+fi
+if [ ! -d ${EZBIDS_WORKING_DIR} ]; then
+  mkdir ${EZBIDS_WORKING_DIR}
+fi
 
 ./generate_keys.sh
 
